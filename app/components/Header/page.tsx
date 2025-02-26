9e5308bfbcf7a6b02fffa9884574620dd1641270
tahminarasulzadeh <92018505+tahminarasulzadeh@users.noreply.github.com>
2025-02-26 13:27:09 +0400

    updating responsive menu

--- app/components/Header/page.tsx
+++ app/components/Header/page.tsx
@@ -45,7 +45,7 @@ export default function HeaderPage() {
             </div>
 
             {/* Mobile Navigation */}
-            <div className="xl:hidden flex w-full sm:justify-start sm:gap-[50px] md:justify-between items-center p-4">
+            <div className="xl:hidden flex w-full  justify-between items-center p-4">
                 <Logo />
                 <RxHamburgerMenu
                     className="text-[45px] cursor-pointer"
